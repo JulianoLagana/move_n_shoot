3a4c9ad76630a33d576f5e1d58cd21aa0c5f1267
Juliano Lagana <juliano@student.chalmers.se>
2017-10-23 16:36:37 +0200

    Makes Player class use float position for everything, but also able to return a Rect for collision detection and drawing.

--- main.py
+++ main.py
@@ -4,32 +4,40 @@ pygame.init()
 
 
 class Player:
+    def __init__(self, sz=None, position=None, img_filename='player.bmp'):
 
-    def __init__(self, sz=None, position=(0, 0), img_filename='player.bmp'):
+        self.START_SPEED = 10
+        self.MAX_SPEED = 40
 
+        # Default value for position
+        if position is None:
+            self.position = [0, 0]
+
+        # Speed and acceleration initializations
+        self.speed = self.START_SPEED
+        self.accel = 1
+
+        # Load image and optionally resize it
         temp = pygame.image.load(img_filename)
         if sz is None:
             self.img = temp
         else:
             self.img = pygame.transform.scale(temp, sz)
 
-        self.rect = self.img.get_rect()
-        self.position = position
-        self.rect.center = position
+    # Returns rectangle around the player image
+    def get_rect(self):
+        r = self.img.get_rect()
+        r.center = (self.position[0], self.position[1])
+        return r
 
-        self.start_speed = 10
-        self.max_speed = 40
-        self.accel = 1
-        self.speed = self.start_speed
-
+    # Draws the player to the screen
     def draw(self, scr):
-        scr.blit(self.img, self.rect)
+        scr.blit(self.img, self.get_rect())
 
 
 size = width, height = 1600, 800
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-
 
 # Initialize dictionary for key presses
 key_pressed = {}
@@ -37,7 +45,7 @@ for key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_UP]:
     key_pressed[key] = False
 
 # Load player
-player = Player((75, 75))
+player = Player([75, 75])
 
 while 1:
     clock.tick(60)
@@ -59,28 +67,29 @@ while 1:
                 key_pressed[event.key] = False
 
     # Update position
-    player.rect.x += player.speed*(key_pressed[pygame.K_RIGHT] - key_pressed[pygame.K_LEFT])
-    player.rect.y += player.speed*(key_pressed[pygame.K_DOWN] - key_pressed[pygame.K_UP])
+    player.position[0] += player.speed*(key_pressed[pygame.K_RIGHT] - key_pressed[pygame.K_LEFT])
+    player.position[1] += player.speed*(key_pressed[pygame.K_DOWN] - key_pressed[pygame.K_UP])
 
     # Check collision
-    if player.rect.left < 0:
-        player.rect.left = 0
-    if player.rect.top < 0:
-        player.rect.top = 0
-    if player.rect.right > width:
-        player.rect.right = width
-    if player.rect.bottom > height:
-        player.rect.bottom = height
+    r = player.get_rect()
+    if r.left < 0:
+        player.position[0] = r.width / 2
+    if r.top < 0:
+        player.position[1] = r.height / 2
+    if r.right > width:
+        player.position[0] = width - r.width/2
+    if r.bottom > height:
+        player.position[1] = height - r.height/2
 
     # Update velocity
     if key_pressed[pygame.K_LEFT] or  \
        key_pressed[pygame.K_RIGHT] or \
        key_pressed[pygame.K_UP] or    \
        key_pressed[pygame.K_DOWN]:
-        if player.speed <= player.max_speed:
+        if player.speed <= player.MAX_SPEED:
             player.speed += 1
     else:
-        player.speed = player.start_speed
+        player.speed = player.START_SPEED
 
     # Update display
     screen.fill((0, 0, 0))
